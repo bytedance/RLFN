79700af422fe8dc8fe99171ffee3d72b9e1316d4
Fangyuan Kong <99717750+fyan111@users.noreply.github.com>
2022-06-09 18:44:53 +0800

    Add files via upload

---
+++ model/rlfn_s.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+# Copyright 2022 ByteDance
+import torch.nn as nn
+from model import block
+
+
+class RLFN_S(nn.Module):
+    """
+    Residual Local Feature Network (RLFN)
+    Model definition of RLFN_S in `Residual Local Feature Network for 
+    Efficient Super-Resolution`
+    """
+
+    def __init__(self,
+                 in_channels=3,
+                 out_channels=3,
+                 feature_channels=48,
+                 upscale=4):
+        super(RLFN_S, self).__init__()
+
+        self.conv_1 = block.conv_layer(in_channels,
+                                       feature_channels,
+                                       kernel_size=3)
+
+        self.block_1 = block.RLFB(feature_channels)
+        self.block_2 = block.RLFB(feature_channels)
+        self.block_3 = block.RLFB(feature_channels)
+        self.block_4 = block.RLFB(feature_channels)
+        self.block_5 = block.RLFB(feature_channels)
+        self.block_6 = block.RLFB(feature_channels)
+
+        self.conv_2 = block.conv_layer(feature_channels,
+                                       feature_channels,
+                                       kernel_size=3)
+
+        self.upsampler = block.pixelshuffle_block(feature_channels,
+                                                  out_channels,
+                                                  upscale_factor=upscale)
+
+    def forward(self, x):
+        out_feature = self.conv_1(x)
+
+        out_b1 = self.block_1(out_feature)
+        out_b2 = self.block_2(out_b1)
+        out_b3 = self.block_3(out_b2)
+        out_b4 = self.block_4(out_b3)
+        out_b5 = self.block_5(out_b4)
+        out_b6 = self.block_6(out_b5)
+
+        out_low_resolution = self.conv_2(out_b6) + out_feature
+        output = self.upsampler(out_low_resolution)
+
+        return output
---
+++ val_demo.py
@@ -0,0 +1,103 @@
+# Copyright (c) 2022 Yawei Li, Kai Zhang, Radu Timofte SPDX license identifier: MIT
+# This file may have been modified by ByteDance
+import os.path
+import logging
+from collections import OrderedDict
+import torch
+
+from utils import utils_logger
+from utils import utils_image as util
+from model.rlfn_ntire import RLFN_Prune
+
+
+def main():
+
+    utils_logger.logger_info('NTIRE2022-EfficientSR', log_path='NTIRE2022-EfficientSR.log')
+    logger = logging.getLogger('NTIRE2022-EfficientSR')
+
+    # --------------------------------
+    # basic settings
+    # --------------------------------
+    # testsets = 'DIV2K 801-900'
+    testsets = os.path.join(os.getcwd(), 'data')
+    testset_L = 'DIV2K_valid_LR_bicubic'
+
+    torch.cuda.current_device()
+    torch.cuda.empty_cache()
+    torch.backends.cudnn.benchmark = True
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    # --------------------------------
+    # load model
+    # --------------------------------
+    model_path = os.path.join('model_zoo', 'rlfn_ntire_x4.pth')
+    model = RLFN_Prune(in_channels=3, out_channels=3)
+    model.load_state_dict(torch.load(model_path), strict=True)
+    model.eval()
+    for k, v in model.named_parameters():
+        v.requires_grad = False
+    model = model.to(device)
+
+    # number of parameters
+    number_parameters = sum(map(lambda x: x.numel(), model.parameters()))
+    logger.info('Params number: {}'.format(number_parameters))
+
+    # --------------------------------
+    # read image
+    # --------------------------------
+    L_folder = os.path.join(testsets, testset_L, 'X4')
+    E_folder = os.path.join(testsets, testset_L+'_results')
+    util.mkdir(E_folder)
+
+    # record PSNR, runtime
+    test_results = OrderedDict()
+    test_results['runtime'] = []
+
+    logger.info(L_folder)
+    logger.info(E_folder)
+    idx = 0
+
+    start = torch.cuda.Event(enable_timing=True)
+    end = torch.cuda.Event(enable_timing=True)
+
+    for img in util.get_image_paths(L_folder):
+
+        # --------------------------------
+        # (1) img_L
+        # --------------------------------
+        idx += 1
+        img_name, ext = os.path.splitext(os.path.basename(img))
+        logger.info('{:->4d}--> {:>10s}'.format(idx, img_name+ext))
+
+        img_L = util.imread_uint(img, n_channels=3)
+        img_L = util.uint2tensor4(img_L) 
+        img_L = img_L * 255.
+        img_L = img_L.to(device)
+
+        start.record()
+        img_E = model(img_L)
+        end.record()
+        torch.cuda.synchronize()
+        test_results['runtime'].append(start.elapsed_time(end))  # milliseconds
+
+
+#        torch.cuda.synchronize()
+#        start = time.time()
+#        img_E = model(img_L)
+#        torch.cuda.synchronize()
+#        end = time.time()
+#        test_results['runtime'].append(end-start)  # seconds
+
+        # --------------------------------
+        # (2) img_E
+        # --------------------------------
+        img_E = img_E / 255.
+        img_E = util.tensor2uint(img_E)
+
+        util.imsave(img_E, os.path.join(E_folder, img_name[:4]+ext))
+    ave_runtime = sum(test_results['runtime']) / len(test_results['runtime']) / 1000.0
+    logger.info('------> Average runtime of ({}) is : {:.6f} seconds'.format(L_folder, ave_runtime))
+
+if __name__ == '__main__':
+
+    main()
